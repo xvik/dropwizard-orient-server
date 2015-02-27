5c32806e8cae4aab85559153e5a8b85c8749f737
Adam Perry <adam.n.perry@gmail.com>
2015-02-26 21:09:47 -0700

    Updated for OrientDB 2.0.3
    Had to remove OMemoryWatchDog from health check, it's no longer part of the OrientDB 2.0 API.

--- src/main/java/ru/vyarus/dropwizard/orient/health/OrientServerHealthCheck.java
+++ src/main/java/ru/vyarus/dropwizard/orient/health/OrientServerHealthCheck.java
@@ -2,7 +2,6 @@ package ru.vyarus.dropwizard.orient.health;
 
 import com.codahale.metrics.health.HealthCheck;
 import com.orientechnologies.orient.core.Orient;
-import com.orientechnologies.orient.core.memory.OMemoryWatchDog;
 
 /**
  * Checks embedded orient server state.
@@ -17,15 +16,10 @@ public class OrientServerHealthCheck extends HealthCheck {
     protected Result check() throws Exception {
         final Orient instance = Orient.instance();
         final boolean noStorages = instance.getStorages().isEmpty();
-        final OMemoryWatchDog memoryWatchDog = instance.getMemoryWatchDog();
-        final boolean noMemory = memoryWatchDog == null
-                || !memoryWatchDog.isMemoryAvailable();
-        Result result;
+
+		Result result;
         if (noStorages) {
             result = Result.unhealthy("No registered storages");
-        } else if (noMemory) {
-            result = Result.unhealthy(String.format("No memory available (used heap %s)",
-                    memoryWatchDog == null ? "unknown" : memoryWatchDog.getUsedHeapMemory()));
         } else {
             result = Result.healthy("OK");
         }
